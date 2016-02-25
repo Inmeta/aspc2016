7a6491931432b930bf4b6fec0aef4a322bf2a834
Ricardo Carton <ritxarin@gmail.com>
2016-02-25 14:39:21 +0100

    Updated Powershell script

--- MarvelCrimeCentral/ProvisionSPOStructure/ProvisionSPOStructure.ps1
+++ MarvelCrimeCentral/ProvisionSPOStructure/ProvisionSPOStructure.ps1
@@ -18,3 +18,22 @@ Add-SPOFieldToContentType -Field "HeroAvailability" -ContentType "Super Hero"
 New-SPOList -Title "Super Hero" -Url "SuperHero" -Template GenericList
 
 Add-SPOContentTypeToList -List "Super Hero" -ContentType "Super Hero" -DefaultContentType
+
+#Alert List
+Add-SPOContentType -Name "Alert" -Description "Use to store alert information" -Group "Marvel Content Types" -ParentContentType $ct
+
+Add-SPOField -DisplayName "Alert ID" -InternalName "AlertID" -Type Text -Group "Marvel CCU" -AddToDefaultView
+Add-SPOField -DisplayName "AlertCoordinatesX" -InternalName "AlertCoordinatesX" -Type Text -Group "Marvel CCU" -AddToDefaultView
+Add-SPOField -DisplayName "AlertCoordinatesY" -InternalName "AlertCoordinatesY" -Type Text -Group "Marvel CCU" -AddToDefaultView
+Add-SPOField -DisplayName "Alert SuperHero" -InternalName "AlertSuperHero" -Type Choice -Group "Marvel CCU" -AddToDefaultView -Choices "Hulk","Thor","Black Widow", "Ironman", "Spiderman"
+Add-SPOField -DisplayName "Alert Status" -InternalName "AlertStatus" -Type Choice -Group "Marvel CCU" -AddToDefaultView -Choices "Received","Assigned","Completed"
+
+Add-SPOFieldToContentType -Field "AlertID" -ContentType "Alert"
+Add-SPOFieldToContentType -Field "AlertCoordinatesX" -ContentType "Alert"
+Add-SPOFieldToContentType -Field "AlertCoordinatesY" -ContentType "Alert"
+Add-SPOFieldToContentType -Field "AlertSuperHero" -ContentType "Alert"
+Add-SPOFieldToContentType -Field "AlertStatus" -ContentType "Alert"
+
+New-SPOList -Title "Alerts" -Url "Alerts" -Template GenericList
+
+Add-SPOContentTypeToList -List "Alerts" -ContentType "Alert" -DefaultContentType
