1578efeb4f1bcdfe45a9171ac3c2a2583c4c88fc
An <Natalia.An@inmeta.no>
2016-02-26 08:51:07 +0100

    data mining for predict crimerate by year and coord - final version

--- R/GetCoordinates.R
+++ R/GetCoordinates.R
@@ -7,7 +7,7 @@ stat = stat[,-1]
 
 komunneName <- as.vector(stat$Komunne)
 crimerate <- stat
-colnames(crimerate) <- c('Name',2014,2013,2012,2011,2010,2009,2008)
+colnames(crimerate) <- c('Name',2008,2009,2010,2011,2012,2013,2014)
 
 coord <- c(0,0,0,0)
 a <- 0
@@ -34,7 +34,7 @@ for (i in (1:length(komunneName)))
     
     for (j in (2008:2014))
     {
-      nr <- cbind(cbind(as.vector(a), crimerate[i,toString(j)]), j-2000)
+      nr <- cbind(cbind(as.vector(a), crimerate[i,toString(j)]), j)
       colnames(nr) <- c("lat", "long","N","Y")
       newrow <- rbind(newrow, nr)
     }
@@ -46,4 +46,4 @@ for (i in (1:length(komunneName)))
 }
 crimeByCoord <- as.matrix(coord[-1,])
 colnames(crimeByCoord) <- c("lat", "long","N","Y")
-write.csv(crimeByCoord, file = "./crimeByCoord.csv", row.names = F, quote = F)
+write.csv(crimeByCoord, file = "./crimeByCoordFullYear.csv", row.names = F, quote = F)
