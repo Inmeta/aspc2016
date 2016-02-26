ec40730d5a05ca657ba6fb90e091eae060461ea4
iladan <natalia.an@inmeta.no>
2016-02-26 09:54:02 +0100

    add example to call AzureML

---
+++ R/GetMLPredictions/GetMlPredictions.Test/TestWebCall.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GetMLPredictions;
+
+namespace GetMlPredictions.Test
+{
+    [TestClass]
+    public class TestWebCall
+    {
+        [TestMethod]
+        public void DoTestCall()
+        {
+            var t = new PredictML();
+            t.GetCrimeNumberByCoordAndYear(51.20304, 12.134566, 2016);
+        }
+    }
+}
