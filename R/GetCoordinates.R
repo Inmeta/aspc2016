f116a332ce1a102ce6ea874281f73f781657a6c3
An <Natalia.An@inmeta.no>
2016-02-26 08:30:13 +0100

    added year to coord

--- R/GetCoordinates.R
+++ R/GetCoordinates.R
@@ -5,13 +5,14 @@ setwd("C:/Git/ASPC2016/R")
 stat <- read.table("C:/Git/ASPC2016/R/FylkeKomunneCrimeNumber.tsv", encoding="UTF-8", sep = "\t", header = T)
 stat = stat[,-1]
 
-
 komunneName <- as.vector(stat$Komunne)
-crimerate <- stat['X2014']
+crimerate <- stat
+colnames(crimerate) <- c('Name',2014,2013,2012,2011,2010,2009,2008)
 
-coord <- c(0,0,0)
+coord <- c(0,0,0,0)
 a <- 0
-i = 12
+i = 1
+j = 1
 for (i in (1:length(komunneName)))
 {
   addr = paste("http://maps.google.com/maps/api/geocode/json?address=", komunneName[i], ",Norway", sep = '')
@@ -25,12 +26,24 @@ for (i in (1:length(komunneName)))
     {
       error <- err
     } )
-  if (is.null(error) && !is.null(a))
+  
+  if (length(a) == 2)
   {
-    newrow <- cbind(as.vector(a), crimerate[i,])
+    #cat(unlist(a))
+    newrow <- c(0,0,0,0)
+    
+    for (j in (2008:2014))
+    {
+      nr <- cbind(cbind(as.vector(a), crimerate[i,toString(j)]), j-2000)
+      colnames(nr) <- c("lat", "long","N","Y")
+      newrow <- rbind(newrow, nr)
+    }
+    newrow <- newrow[-1,]
+    
     coord <- rbind(coord, newrow)
   }
+  closeAllConnections()
 }
 crimeByCoord <- as.matrix(coord[-1,])
-colnames(crimeByCoord) <- c("lat", "long","N")
-write.csv(crimeByCoord, file = "./crimeByCoord.csv")
+colnames(crimeByCoord) <- c("lat", "long","N","Y")
+write.csv(crimeByCoord, file = "./crimeByCoord.csv", row.names = F, quote = F)
